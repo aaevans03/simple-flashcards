168ff8b3613a11cade7daafceea4c13fe03c6495
aaevans03 <aaevans03@gmail.com>
2025-10-02 12:32:56 -0600

    Added event listeners to flip card when user presses up/down key

--- src/flashcard/flashcard.jsx
+++ src/flashcard/flashcard.jsx
@@ -4,8 +4,21 @@ import '../app.css';
 export function Flashcard({term, definition}) {
     const [flipped, setFlipped] = React.useState(false);
 
+    React.useEffect(() => {
+        // event listener for arrow up/down keys to flip card
+        const onKeyDown = (event) => {
+            if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+                flipCard();
+            }
+        };
+        window.addEventListener("keydown", onKeyDown);
+        
+        // remove event listener when component unmounts
+        return () => window.removeEventListener("keydown", onKeyDown);
+    }, []);
+
     function flipCard() {
-        setFlipped(!flipped)
+        setFlipped(f => !f);
     }
 
     return (<div className="card" onClick={flipCard}>{flipped ? definition : term}</div>);

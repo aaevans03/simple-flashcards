53b2636b62e704ca5d06ded70a90a50904d6c3d8
aaevans03 <aaevans03@gmail.com>
2025-10-13 17:05:01 -0600

    Input field for flashcards with basic formatting

---
+++ src/flashcardCreator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function FlashcardCreator() {
+
+    // input field
+    // parser code
+    // save button
+    // save to local storage
+
+    return (
+        <textarea type="text" className="input" />
+    );
+}

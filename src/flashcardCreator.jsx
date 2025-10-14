b6a4109e0409ace4e98bf0e1bf7666696d474188
aaevans03 <aaevans03@gmail.com>
2025-10-13 22:46:42 -0600

    Cleaning up of flashcard files: removes undefined or null entries

--- src/flashcardCreator.jsx
+++ src/flashcardCreator.jsx
@@ -3,33 +3,41 @@ import React from 'react';
 export default function FlashcardCreator({ setFlashcards, returnToViewer }) {
 
     const [userInput, setUserInput] = React.useState(() => {
+        // load from local storage
         const savedFlashcards = JSON.parse(localStorage.getItem("flashcards"));
         if (!savedFlashcards) return "";
+
+        // clean up any bad entries
+        const cleanedFlashcards = removeBadEntries(savedFlashcards);
+
+        // create a new output to be loaded into text area
         let output = "";
-        for (const line of savedFlashcards) {
-            // TODO: fix blank lines being added
-            // console.log(line);
+        for (const line of cleanedFlashcards) {
             output += line.term + " - " + line.definition + "\n";
         }
-        // console.log(output);
         return output;
     });
 
-    // input field
-    // parser code
-    // save button
-    // save to local storage
-
+    // save flashcards to local storage, send to parent
     function saveFlashcards() {
         // split by new line, then split by " - "
         const parsedFlashcards = userInput.split("\n").map(line => {
             const [term, definition] = line.split(" - ");
             return { term, definition };
         });
-        // TODO: remove blank lines, nulls/undefineds
-        localStorage.setItem("flashcards", JSON.stringify(parsedFlashcards));
-        setFlashcards(parsedFlashcards);
+
+        // clean up any bad entries
+        const cleanedFlashcards = removeBadEntries(parsedFlashcards);
+
+        // add to local storage and send to flashcard viewer
+        localStorage.setItem("flashcards", JSON.stringify(cleanedFlashcards));
+        setFlashcards(cleanedFlashcards);
         returnToViewer();
+    }
+
+    // remove any flashcard entries with null or undefined terms
+    function removeBadEntries(input) {
+        return input.filter(card => card.term && card.definition);
     }
 
     return (

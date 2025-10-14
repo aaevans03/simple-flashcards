c7e11ccb35006f9cdc12d0faaec76b84dd964a8f
aaevans03 <aaevans03@gmail.com>
2025-10-13 23:10:16 -0600

    Definition side more obvious

--- src/flashcard/flashcard.jsx
+++ src/flashcard/flashcard.jsx
@@ -3,7 +3,7 @@ import '../app.css';
 
 export function Flashcard({card, flipCard, flipped}) {
     return (
-        <div className="card" onClick={flipCard}>
+        <div className={`card ${flipped ? "flipped" : ""}`} onClick={flipCard}>
             {flipped ? card.definition : card.term}
         </div>
     );
